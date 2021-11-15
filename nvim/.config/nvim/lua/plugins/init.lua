c3bb682238cac80e66ab984a61f757135b10c624
yuanyuyuan <az6980522@gmail.com>
2021-11-15 22:33:15 +0800

    [nvim] add mapx

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -2,6 +2,7 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
+  use 'b0o/mapx.nvim'
   use { 'lewis6991/gitsigns.nvim',
     config = function()
       require 'gitsigns'.setup()
