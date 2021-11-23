fcad7007901f6a9a10723557368e7c24f5eff54c
yuanyuyuan <az6980522@gmail.com>
2021-11-23 08:59:06 +0800

    [nvim] resolve slow speed in git diff by changing galaxyline to lualine

--- nvim/.config/nvim/lua/plugins/statusline.lua
+++ nvim/.config/nvim/lua/plugins/statusline.lua
@@ -1,173 +1,121 @@
-local gl = require('galaxyline')
-local condition = require('galaxyline.condition')
-local gls = gl.section
-gl.short_line_list = {'NvimTree', 'packer', 'vista', 'dbui'}
-
+-- Color for highlights
 local colors = {
-  bg = '#282c34',
-  crayola_yellow = '#fce883',
-  yellow = '#fabd2f',
+  yellow = '#f9e79f',
   cyan = '#008080',
   darkblue = '#081633',
-  green = '#afd700',
+  green = '#76d7c4',
   orange = '#FF8800',
-  purple = '#5d4d7a',
-  magenta = '#d16d9e',
-  grey = '#c0c0c0',
-  blue = '#0087d7',
-  red = '#ec5f67'
+  violet = '#a9a1e1',
+  magenta = '#c678dd',
+  blue = '#51afef',
+  red = '#ec7063'
 }
 
-local buffer_not_empty = function()
-  if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then
-    return true
-  end
-  return false
-end
-
-local checkwidth = function()
-  local squeeze_width  = vim.fn.winwidth(0) / 2
-  if squeeze_width > 40 then
-    return true
-  end
-  return false
-end
-
-gls.left = {
-  { ViMode = {
-    provider = function()
-      local alias = {
-        n = 'NORMAL',
-        i = 'INSERT',
-        c = 'COMMAND',
-        v = 'VISUAL',
-        V = 'VISUAL LINE',
-        [''] = 'VISUAL BLOCK',
-        s = 'SELECT',
-        S = 'SELECT',
-        r = 'REPLACE',
-        R = 'REPLACE',
+require'lualine'.setup {
+  options = {
+    icons_enabled = true,
+    theme = 'material',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {},
+    always_divide_middle = true,
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {
+      'filetype',
+      {
+        'diagnostics',
+        sources={'nvim_lsp'},
+        symbols = {
+          error = ' ',
+          warn = ' ',
+          info = ' ',
+          hint = ' '
+        },
+      },
+    },
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,
+        path = 0,
+      },
+      -- function() return require"lsp-status".status() end,
+      {
+        'lsp_progress',
+        display_components = {
+          'lsp_client_name',
+          'spinner',
+          {
+            'title',
+            'percentage',
+            'message'
+          }
+        },
+        colors = {
+          lsp_client_name = colors.yellow,
+          spinner = colors.blue,
+          title  = colors.red,
+          percentage  = colors.red,
+          message  = colors.red,
+          use = true,
+        },
+        separators = {
+          component = ' ',
+          progress = ' | ',
+          percentage = { pre = '', post = '%% ' },
+          title = { pre = '', post = ': ' },
+          lsp_client_name = { pre = '[', post = ']' },
+          spinner = { pre = '', post = '' },
+          message = {
+            commenced = 'In Progress',
+            completed = 'Completed',
+            pre = '(', post = ')'
+          },
+        },
+        timer = {
+          progress_enddelay = 500,
+          spinner = 500,
+          lsp_client_name_enddelay = 500
+        },
+        spinner_symbols = {
+          '⠋',
+          '⠙',
+          '⠹',
+          '⠸',
+          '⠼',
+          '⠴',
+          '⠦',
+          '⠧',
+          '⠇',
+          '⠏'
+        },
       }
-      return '  ' .. alias[vim.fn.mode()]
-    end,
-    separator = ' ',
-    separator_highlight = {colors.purple,function()
-      if not buffer_not_empty() then
-        return colors.purple
-      end
-      return colors.darkblue
-    end},
-    highlight = {colors.darkblue,colors.purple,'bold'},
-  }},
-  { FileIcon = {
-    provider = 'FileIcon',
-    condition = buffer_not_empty,
-    highlight = {
-      require('galaxyline.provider_fileinfo').get_file_icon_color,
-      colors.darkblue
     },
-  }},
-  { FileFormat = {
-    provider = {
-      function () return vim.bo.filetype .. " " end,
-      'LineColumn'
+    lualine_x = {'progress'},
+    lualine_y = {
+      {
+        'diff',
+        symbols = {
+          added = ' ',
+          modified = '柳',
+          removed = ' '
+        }
+      }
     },
-    condition = buffer_not_empty,
-    separator = ' ',
-    highlight = {colors.magenta,colors.darkblue},
-    separator_highlight = {colors.bg,colors.darkblue},
-  }},
-  -- { FileName = {
-  --   provider = {'FileName', 'LineColumn'},
-  --   condition = buffer_not_empty,
-  --   separator = ' ',
-  --   highlight = {colors.magenta,colors.darkblue},
-  --   separator_highlight = {colors.bg,colors.darkblue},
-  -- }},
-  -- { FilePath = {
-  --   provider = function() return vim.fn.expand("%") end,
-  --   -- provider = function() return vim.fn.pathshorten(vim.fn.getcwd()) end,
-  --   condition = buffer_not_empty,
-  --   separator = ' ',
-  --   highlight = {colors.magenta,colors.darkblue},
-  --   separator_highlight = {colors.bg,colors.darkblue},
-  -- }},
-  -- { LeftEnd = {
-  --   provider = function() return '' end,
-  --   separator = ' ',
-  --   separator_highlight = {colors.purple,colors.bg},
-  --   -- highlight = {colors.purple,colors.purple}
-  -- }},
-  { DiagnosticError = {
-    provider = 'DiagnosticError',
-    icon = '  ',
-    highlight = {colors.red,colors.bg}
-  }},
-  { DiagnosticWarn = {
-    provider = 'DiagnosticWarn',
-    icon = '  ',
-    highlight = {colors.yellow,colors.bg},
-  }},
-  { LspStatus = {
-    provider = function() return require('lsp-status').status() end,
-    highlight = {colors.crayola_yellow,colors.bg, 'bold'},
-  }},
+    lualine_z = {
+      'branch',
+    }
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {'nvim-tree'}
 }
-gls.right = {
-  { GitIcon = {
-    provider = function() return '  ' end,
-    condition = condition.check_git_workspace,
-    highlight = {colors.orange,colors.purple},
-    separator = '',
-    separator_highlight = {colors.bg,colors.purple},
-  }},
-  { GitBranch = {
-    provider = 'GitBranch',
-    condition = condition.check_git_workspace,
-    highlight = {colors.grey,colors.purple},
-  }},
-  { DiffAdd = {
-      provider = 'DiffAdd',
-      condition = checkwidth,
-      icon = ' ',
-      highlight = {colors.green,colors.purple},
-  }},
-  { DiffModified = {
-    provider = 'DiffModified',
-    -- condition = checkwidth,
-    condition = condition.check_git_workspace,
-    icon = ' ',
-    highlight = {colors.orange,colors.purple},
-  }},
-  { DiffRemove = {
-    provider = 'DiffRemove',
-    -- condition = checkwidth,
-    condition = condition.check_git_workspace,
-    icon = ' ',
-    highlight = {colors.red,colors.purple},
-  }},
-  { PerCent = {
-    provider = {'LinePercent'},
-    separator = ' ' ,
-    separator_highlight = {colors.darkblue, colors.purple},
-    highlight = {colors.grey, colors.darkblue},
-  }},
-}
-gls.short_line_left = {
-  { BufferType = {
-    provider = 'FileTypeName',
-    separator = '  ',
-    separator_highlight = {colors.purple,colors.bg},
-    highlight = {colors.grey,colors.purple}
-  }},
-}
-gls.short_line_right = {
-  { BufferIcon = {
-    provider = 'BufferIcon',
-    separator = ' ',
-    separator_highlight = {colors.purple,colors.bg},
-    highlight = {colors.grey,colors.purple}
-  }},
-}
-
-return gls
