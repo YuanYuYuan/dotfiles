6f35fd0a1ac8be05e690000105d11bf7b0f17843
yuanyuyuan <az6980522@gmail.com>
2021-11-24 22:54:35 +0800

    [nvim] add yank path mapping

--- nvim/.config/nvim/lua/keymappings.lua
+++ nvim/.config/nvim/lua/keymappings.lua
@@ -1,4 +1,14 @@
 local map = vim.api.nvim_set_keymap
+local M = {}
 
-map('n', '<Space>rt', '<Cmd>!alacritty --working-directory %:p:h&<CR>', {noremap = true, silent=true})
-map('n', '<Space>rl', '<Cmd>LspStart<CR>', {noremap = true})
+function M.yank_and_display_path()
+  local file_path = vim.fn.expand('%:p')
+  vim.api.nvim_command("call setreg('+', '" .. file_path.. "')")
+  print(file_path)
+end
+
+map('n', '<Space><Space>t', '<Cmd>!alacritty --working-directory %:p:h&<CR>', {noremap = true, silent=true})
+map('n', '<Space><Space>l', '<Cmd>LspStart<CR>', {noremap = true})
+map('n', '<C-g>', "<Cmd>lua require('keymappings').yank_and_display_path()<CR>", {noremap = true, silent=true})
+
+return M
