80095653c2575454afc8dec5e855eb6367c3b9ab
yuanyuyuan <az6980522@gmail.com>
2022-02-11 15:49:46 +0800

    [nvim] update vsnip

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,7 +1,7 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
 return require('packer').startup(function(use)
@@ -83,7 +83,7 @@ return require('packer').startup(function(use)
     end
   }
   use { 'iamcco/markdown-preview.nvim',
-    ft = "markdown",
+    ft = 'markdown',
     run = 'cd app && yarn install',
     config = vim.cmd [[
       let g:mkdp_auto_close = 0
@@ -334,7 +334,7 @@ return require('packer').startup(function(use)
   }
 
   -- Automatically set up your configuration after cloning packer.nvim
-  if packer_bootstrap then
+  if PACKER_BOOTSTRAP then
     require('packer').sync()
   end
 end)
