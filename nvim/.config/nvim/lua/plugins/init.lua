7f75301d0ff7e31767af8468b18d5c55c491e64d
yuanyuyuan <az6980522@gmail.com>
2022-04-07 14:15:54 +0800

    [nvim] fix vimtex mathzone bug

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -236,13 +236,14 @@ return require('packer').startup(function(use)
       " avoid plaintex
       let g:tex_flavor='latex'
 
-      let g:vimtex_view_method = 'zathura'
-
       " enable folding
       " let g:vimtex_fold_manual=0
       let g:vimtex_fold_enabled=1
 
       let g:vimtex_quickfix_enabled = 0
+
+      " fix bug of mathznoe, ref: https://github.com/lervag/vimtex/issues/2346
+      autocmd BufNewFile,BufRead *.tex so $VIMRUNTIME/syntax/tex.vim
 
       autocmd BufWinLeave *.tex silent! VimtexClean
     ]]
