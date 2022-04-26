8e4b751df6858c8ed701d894cba4ff37492ccc8e
yuanyuyuan <az6980522@gmail.com>
2022-04-26 22:23:04 +0800

    [nvim] tidy bufferline

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -5,16 +5,24 @@ require'bufferline'.setup{
   }
 }
 
-vim.api.nvim_set_keymap("n", "<C-l>", ":BufferLineCycleNext<CR>", {silent = true})
-vim.api.nvim_set_keymap("n", "<C-h>", ":BufferLineCyclePrev<CR>", {silent = true})
-vim.api.nvim_set_keymap("n", "L", ":BufferLineMoveNext<CR>", {silent = true})
-vim.api.nvim_set_keymap("n", "H", ":BufferLineMovePrev<CR>", {silent = true})
-vim.api.nvim_set_keymap("n", "<Space>1", "<Cmd>BufferLineGoToBuffer 1<CR>", {silent = true})
-vim.api.nvim_set_keymap("n", "<Space>2", "<Cmd>BufferLineGoToBuffer 2<CR>", {silent = true})
-vim.api.nvim_set_keymap("n", "<Space>3", "<Cmd>BufferLineGoToBuffer 3<CR>", {silent = true})
-vim.api.nvim_set_keymap("n", "<Space>4", "<Cmd>BufferLineGoToBuffer 4<CR>", {silent = true})
-vim.api.nvim_set_keymap("n", "<Space>5", "<Cmd>BufferLineGoToBuffer 5<CR>", {silent = true})
-vim.api.nvim_set_keymap("n", "<Space>6", "<Cmd>BufferLineGoToBuffer 6<CR>", {silent = true})
-vim.api.nvim_set_keymap("n", "<Space>7", "<Cmd>BufferLineGoToBuffer 7<CR>", {silent = true})
-vim.api.nvim_set_keymap("n", "<Space>8", "<Cmd>BufferLineGoToBuffer 8<CR>", {silent = true})
-vim.api.nvim_set_keymap("n", "<Space>9", "<Cmd>BufferLineGoToBuffer 9<CR>", {silent = true})
+local mappings = {
+  ['<C-l>'] = 'BufferLineCycleNext',
+  ['<C-h>'] = 'BufferLineCyclePrev',
+  ['L'] = 'BufferLineMoveNext',
+  ['H'] = 'BufferLineMovePrev',
+}
+
+for key, fcn in pairs(mappings) do
+  vim.keymap.set('n', key, '<cmd>' .. fcn .. '<cr>', {noremap = true, silent = true})
+end
+
+
+local cmd = require('bufferline.commands')
+for idx = 1, 9 do
+  vim.keymap.set(
+    'n',
+    '<Space>' .. idx,
+    function () cmd.go_to(idx) end,
+    {noremap = true, silent = true}
+  )
+end
