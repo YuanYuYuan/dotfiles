d5f228808579051f1a4d8a8342659a0a5ab67628
yuanyuyuan <az6980522@gmail.com>
2022-10-18 13:24:54 +0800

    [nvim] trans and speak

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -15,3 +15,22 @@ require('toggleterm').setup{
 }
 
 vim.keymap.set({'t'}, '<Esc>', '<C-\\><C-n>')
+
+local speak_and_trans = function(text)
+  local prev_win = vim.api.nvim_get_current_win()
+  local prev_pos = vim.api.nvim_win_get_cursor(prev_win)
+  require('toggleterm').exec(string.format('trans -b -speak -t zh-TW "%s"', text))
+  vim.api.nvim_win_set_cursor(prev_win, prev_pos)
+  vim.api.nvim_set_current_win(prev_win)
+end
+
+-- speak & translate selection
+vim.api.nvim_create_autocmd('Filetype', {
+  pattern = 'text',
+  callback = function()
+    vim.keymap.set({'v'}, '<F3>', function() speak_and_trans(require('utils').get_visual_selection()) end)
+    vim.keymap.set({'n'}, '<F3>', function() speak_and_trans(vim.api.nvim_get_current_line()) end)
+    vim.keymap.set({'v'}, '<Space>3', function() speak_and_trans(require('utils').get_visual_selection()) end)
+    vim.keymap.set({'n'}, '<Space>3', function() speak_and_trans(vim.api.nvim_get_current_line()) end)
+  end,
+})
--- nvim/.config/nvim/lua/utils.lua
+++ nvim/.config/nvim/lua/utils.lua
@@ -45,6 +45,21 @@ function table.map(tbl, fcn)
   return tbl
 end
 
+
+-- https://github.com/nvim-telescope/telescope.nvim/issues/1923#issuecomment-1123136065
+M.get_visual_selection = function()
+  vim.cmd('noau normal! "vy"')
+  local text = vim.fn.getreg('v')
+  vim.fn.setreg('v', {})
+
+  text = string.gsub(text, '\n', '')
+  if #text > 0 then
+    return text
+  else
+    return ''
+  end
+end
+
 return M
 
 
