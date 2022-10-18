1a00d0cb42660dc844c97f1d4c8aca3f9338a239
yuanyuyuan <az6980522@gmail.com>
2022-10-18 13:25:19 +0800

    [nvim] Update for nvim 0.9

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -134,7 +134,7 @@ local servers = {
 
 
 -- from cmp_nvim_lsp
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 for server, config in pairs(servers) do
   config.autostart = true
