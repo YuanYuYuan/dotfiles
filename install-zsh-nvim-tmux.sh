a8731781ce3b3a9ec40e93bbadde1865b08ffb10
yuanyuyuan <az6980522@gmail.com>
2023-04-18 14:00:24 +0800

    Add a testing auto installation script

---
+++ install-zsh-nvim-tmux.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+cd $HOME
+apt update -y
+apt install -y stow git zsh tmux curl gcc g++
+git clone https://github.com/YuanYuYuan/dotfiles
+cd dotfiles
+
+# tmux
+stow tmux
+ln -s $HOME/.config/tmux.conf $HOME/.tmux.conf
+
+# zsh
+cd $(git rev-parse --show-toplevel)
+./zsh/install.sh
+
+# nvim
+cd $(git rev-parse --show-toplevel)
+./nvim/download.sh
