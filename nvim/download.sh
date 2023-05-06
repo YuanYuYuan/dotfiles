f9edcd37377ee9e46dc3e09574178035759d88ee
yuanyuyuan <az6980522@gmail.com>
2023-05-06 19:36:08 +0800

    [nvim] remove packer installation

--- nvim/download.sh
+++ nvim/download.sh
@@ -12,17 +12,6 @@ base_dir=$HOME/.local
 mkdir -p $base_dir
 curl -L https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz | tar -xz --strip 1 -C $base_dir
 
-# Install packer
-packer_dir="$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
-if [ -d $packer_dir ]; then
-    echo ">>> Pulling the lateset packer ..."
-    cd $packer_dir
-    git pull --depth 1
-else
-    echo ">>> Cloneing packer source code ..."
-    git clone --depth 1 https://github.com/wbthomason/packer.nvim $packer_dir
-fi
-
 # Relocate to the project root
 cd $(git rev-parse --show-toplevel)
 stow nvim
