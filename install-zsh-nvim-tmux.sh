3e7989c7313c7799727030026b5b7962ca53e17b
yuanyuyuan <az6980522@gmail.com>
2023-05-07 23:20:50 +0800

    Update installation script

--- install-zsh-nvim-tmux.sh
+++ install-zsh-nvim-tmux.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 cd $HOME
-apt update -y
-apt install -y stow git zsh tmux curl gcc g++
+
+[ "$EUID" == 0 ] && {
+    apt update -y
+    apt install -y stow git zsh tmux curl gcc g++
+}
+
 git clone https://github.com/YuanYuYuan/dotfiles
 cd dotfiles
 
