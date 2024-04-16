10631de4a10a8df8816946d0160ff8046ef3e654
yuanyuyuan <az6980522@gmail.com>
2024-04-16 18:55:38 +0800

    [nvim] add downloading script for the nightly version

---
+++ nvim/download-nightly.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+for pkg in curl clang; do
+    command -v $pkg &> /dev/null || {
+        echo "$pkg not installed!"
+        exit
+    }
+done
+
+# Download the latest release of nvim
+base_dir=$HOME/.local
+mkdir -p $base_dir
+curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz | tar -xz --strip 1 -C $base_dir
+
+# Relocate to the project root
+cd $(git rev-parse --show-toplevel)
+stow nvim
+
+# Install packages
+$HOME/.local/bin/nvim --headless "+Lazy! sync" +qa
