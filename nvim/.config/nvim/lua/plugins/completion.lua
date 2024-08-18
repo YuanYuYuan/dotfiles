ebcb35aa19b389d6eae80e86806fbadf98210398
yuanyuyuan <az6980522@gmail.com>
2024-08-19 01:33:15 +0800

    [nvim]: fix the completion kind bug

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -26,21 +26,31 @@ return {
           documentation = cmp.config.window.bordered(),
         },
         formatting = {
-          format = function(entry, vim_item)
-            vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
+          format = require("lspkind").cmp_format({
+            mode = 'symbol', -- show only symbol annotations
+            maxwidth = 50,   -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            -- can also be a function to dynamically calculate max width such as
+            -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+            ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+            show_labelDetails = true, -- show labelDetails in menu. Disabled by default
 
-            vim_item.menu = ({
-              nvim_lsp = "[LSP]",
-              buffer = "[Buffer]",
-              nvim_lua = "[Lua]",
-              luasnip = "[Luasnip]",
-              path = "[Path]",
-              calc = "[Calc]",
-              crates = "[Crates]",
-            })[entry.source.name]
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            before = function(entry, vim_item)
+              -- vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
+              vim_item.menu = ({
+                nvim_lsp = "[LSP]",
+                buffer = "[Buffer]",
+                nvim_lua = "[Lua]",
+                luasnip = "[Luasnip]",
+                path = "[Path]",
+                calc = "[Calc]",
+                crates = "[Crates]",
+              })[entry.source.name]
+              return vim_item
+            end,
+          })
 
-            return vim_item
-          end,
         },
         mapping = {
           ["<C-p>"] = cmp.mapping.select_prev_item(),
@@ -51,7 +61,7 @@ return {
 
           -- https://github.com/hrsh7th/cmp-cmdline/issues/42
           -- ["<CR>"] = cmp.mapping.confirm({ select = false }),
-          ['<CR>'] = cmp.mapping(cmp.mapping.confirm({ select = true })),
+          ["<CR>"] = cmp.mapping(cmp.mapping.confirm({ select = true })),
 
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
@@ -91,7 +101,7 @@ return {
         mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources({
           { name = "path" },
-      }, {
+        }, {
           {
             name = "cmdline",
             option = {
