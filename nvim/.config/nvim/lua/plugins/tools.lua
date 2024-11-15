e485ac1c43df014b15b46bb2a30998f5800adfc6
yuanyuyuan <az6980522@gmail.com>
2024-11-16 00:28:13 +0800

    [nvim] remove redundant git configs

--- nvim/.config/nvim/lua/plugins/tools.lua
+++ nvim/.config/nvim/lua/plugins/tools.lua
@@ -1,33 +1,5 @@
 return {
   {
-    "sindrets/diffview.nvim",
-    config = function()
-      local actions = require("diffview.actions")
-      require("diffview").setup({
-        keymaps = {
-          view = {
-            { "n", "<Space>ch",  actions.conflict_choose("ours") },
-            { "n", "<Space>cl",  actions.conflict_choose("theirs") },
-            { "n", "<Space>cn",  actions.next_conflict },
-            { "n", "<Space>cp",  actions.prev_conflict },
-            { "n", "<Space>cb",  actions.toggle_files },
-            { "n", "<Space>cx",  actions.cycle_layout },
-          }
-        }
-      })
-    end
-  },
-  {
-    "tpope/vim-fugitive",
-    config = function()
-      require("utils").bind_mappings({
-        ["<Space>gv"] = "<Cmd>Gvdiffsplit<CR>",
-        ["<Space>gb"] = "<Cmd>G Blame<CR>",
-      })
-    end
-
-  },
-  {
     "iamcco/markdown-preview.nvim",
     ft = "markdown",
     build = "cd app && yarn install",
