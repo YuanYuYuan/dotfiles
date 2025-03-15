328cff0049fb6a9262bccdd6ac3b7294da66dc02
yuanyuyuan <az6980522@gmail.com>
2025-03-15 16:14:04 +0800

    refacotr(nvim): improve the translation

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -21,11 +21,11 @@ return {
 
       vim.keymap.set({ "t" }, "<Esc>", "<C-\\><C-n>")
 
-      local speak_and_trans = function(text)
+      local speak_and_trans = function(text, lang)
+        lang = lang or "en-US"
         local prev_win = vim.api.nvim_get_current_win()
         local prev_pos = vim.api.nvim_win_get_cursor(prev_win)
-        -- require("toggleterm").exec(string.format('trans -b -speak -s en-US -t zh-TW "%s"', text))
-        require("toggleterm").exec(string.format('trans -b -speak -s fr -t zh-TW "%s"', text))
+        require("toggleterm").exec(string.format('trans -b -speak -s %s -t zh-TW "%s"', lang, text))
         vim.api.nvim_win_set_cursor(prev_win, prev_pos)
         vim.api.nvim_set_current_win(prev_win)
       end
@@ -34,18 +34,20 @@ return {
       vim.api.nvim_create_autocmd("Filetype", {
         pattern = "text",
         callback = function()
-          vim.keymap.set({ "v" }, "<F3>", function()
-            speak_and_trans(require("utils").get_visual_selection())
-          end)
-          vim.keymap.set({ "n" }, "<F3>", function()
-            speak_and_trans(vim.api.nvim_get_current_line())
-          end)
-          vim.keymap.set({ "v" }, "<Space>3", function()
-            speak_and_trans(require("utils").get_visual_selection())
-          end)
-          vim.keymap.set({ "n" }, "<Space>3", function()
-            speak_and_trans(vim.api.nvim_get_current_line())
-          end)
+          local key_lang_map = {
+            ["<F3>"] = "en-US",
+            ["<Space>3"] = "en-US",
+            ["<F4>"] = "fr",
+            ["<Space>4"] = "fr",
+          }
+          for key, lang in pairs(key_lang_map) do
+            vim.keymap.set({ "v" }, key, function()
+              speak_and_trans(require("utils").get_visual_selection(), lang)
+            end)
+            vim.keymap.set({ "n" }, key, function()
+              speak_and_trans(vim.api.nvim_get_current_line(), lang)
+            end)
+          end
         end,
       })
 
