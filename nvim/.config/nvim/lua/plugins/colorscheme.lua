30605cc612745ebe792eea0b68e3081e9c804e59
yuanyuyuan <az6980522@gmail.com>
2025-10-04 01:16:11 +0800

    feat(nvim): migrate to catppuccin theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,29 +1,85 @@
 return {
-  {
-    "EdenEast/nightfox.nvim",
-    -- Load it during startup
-    lazy = false,
-    -- Load this before all the other plugins
-    priority = 1000,
-    config = function()
-      require("nightfox").setup({
-        options = {
-          transparent = true,
-          styles = {
-            comments = "italic",
-            keywords = "bold",
-            types = "bold",
-          },
-        },
-        groups = {
-          all = {
-            Folded = { bg = "none" },
-            Comment = { fg = "#33cccc" },
-            Visual = { bg = "#01779c" },
-          },
-        },
-      })
-      vim.cmd("colorscheme nightfox")
-    end,
-  },
+    -- {
+    --   "EdenEast/nightfox.nvim",
+    --   -- Load it during startup
+    --   lazy = false,
+    --   -- Load this before all the other plugins
+    --   priority = 1000,
+    --   config = function()
+    --     require("nightfox").setup({
+    --       options = {
+    --         transparent = true,
+    --         styles = {
+    --           comments = "italic",
+    --           keywords = "bold",
+    --           types = "bold",
+    --         },
+    --       },
+    --       groups = {
+    --         all = {
+    --           Folded = { bg = "none" },
+    --           Comment = { fg = "#33cccc" },
+    --           Visual = { bg = "#01779c" },
+    --         },
+    --       },
+    --     })
+    --     vim.cmd("colorscheme nightfox")
+    --   end,
+    -- },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "macchiato", -- latte, frappe, macchiato, mocha
+                -- background = { -- :h background
+                --     light = "latte",
+                --     dark = "mocha",
+                -- },
+                transparent_background = true, -- disables setting the background color.
+                show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+                term_colors = false,     -- sets terminal colors (e.g. `g:terminal_color_0`)
+                dim_inactive = {
+                    enabled = false,     -- dims the background color of inactive window
+                    shade = "dark",
+                    percentage = 0.15,   -- percentage of the shade to apply to the inactive window
+                },
+                no_italic = false,       -- Force no italic
+                no_bold = false,         -- Force no bold
+                no_underline = false,    -- Force no underline
+                styles = {               -- Handles the styles of general hi groups (see `:h highlight-args`):
+                    comments = { "italic" }, -- Change the style of comments
+                    conditionals = { "italic" },
+                    loops = {},
+                    functions = {},
+                    keywords = {},
+                    strings = {},
+                    variables = {},
+                    numbers = {},
+                    booleans = {},
+                    properties = {},
+                    types = {},
+                    operators = {},
+                    -- miscs = {}, -- Uncomment to turn off hard-coded styles
+                },
+                color_overrides = {},
+                custom_highlights = {},
+                default_integrations = true,
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    nvimtree = true,
+                    treesitter = true,
+                    notify = false,
+                    mini = {
+                        enabled = true,
+                        indentscope_color = "",
+                    },
+                    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+                },
+            })
+            vim.cmd("colorscheme catppuccin")
+        end
+    },
 }
